080e060be1707be379b03cba20999ed46a5c6f91
Alexander Jia <jayjialex@gmail.com>
2017-02-28 18:34:53 -0500

    initial player movement + camera position implemented
    Fixed the camera to follow the player in 'first person'.
    Changed the controls slightly to be more 'mech'ish, but am likely to add
    more in the future:
    left joystick controls movement
    right joystick controls turning.
    implemented left and right trigger functionality (they literally do
    nothing for now, though).

--- ZeroVR/Assets/_Scripts/PlayerController.cs
+++ ZeroVR/Assets/_Scripts/PlayerController.cs
@@ -4,30 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	private Vector3 movementVector;
+	private float verticalMove, horizontalMove, horizontalTurn;
 	private CharacterController characterController;
+
+	//test values.
+	public Camera mainCamera;
+//	public Camera secondaryCamera;
 	public float moveSpeed = 8;
+	public float turnSpeed = 6;
 	public float jumpPower = 16;
-	public float gravity = 40;
+	public float gravity = 9.8f;
+	private bool fireLeft = false;
+	private bool fireRight = false;
 
 	// Use this for initialization
 	void Start () {
 		characterController = GetComponent<CharacterController>();
+		mainCamera.enabled = true;
+//		secondaryCamera.enabled = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		movementVector.x = Input.GetAxis ("LeftJoystickX") * moveSpeed;
-		movementVector.z = Input.GetAxis ("LeftJoystickY") * moveSpeed;
+		//Player Movement
+
 
 		if (characterController.isGrounded) {
 			movementVector.y = 0; //resets the dood.
+			movementVector = transform.forward * Input.GetAxis ("LeftJoystickY") * moveSpeed;
+			horizontalTurn = Input.GetAxis ("RightJoystickX") * turnSpeed;
+
+			transform.Rotate (new Vector3 (0, horizontalTurn, 0));
 			if (Input.GetButtonDown ("A")) {
 				movementVector.y = jumpPower;
 			}
 		}
 		movementVector.y -= gravity * Time.deltaTime;
+		characterController.Move(movementVector * Time.deltaTime);
+	
 
-		characterController.Move (movementVector * Time.deltaTime);
+	//	movementVector.x = Input.GetAxis ("LeftJoystickX") * moveSpeed;
+	//	movementVector.z = Input.GetAxis ("LeftJoystickY") * moveSpeed;
+
+
+	//	characterController.Move (movementVector * Time.deltaTime);
+
+		if (Input.GetAxis ("LeftTrigger") > 0) {
+			fireLeft = true;
+			Debug.Log ("fireLeft: " + fireLeft);
+		} 
+		if (Input.GetAxis ("RightTrigger") > 0) {
+			fireRight = true;
+			Debug.Log ("fireRight: " + fireRight);
+		} 
+//		if (Input.GetButtonDown ("Start Button")) {
+//				mainCamera.enabled = false;
+//				secondaryCamera.enabled = true;
+//			Debug.Log ("mainCam is currently: " + mainCamera.enabled + ", secondaryCam is: " + secondaryCamera.enabled);
+//		}
+//		if (Input.GetButtonDown ("Select Button")) {
+//			{
+//				mainCamera.enabled = true;
+//				secondaryCamera.enabled = false;
+//			}
+//			Debug.Log ("mainCam is currently: " + mainCamera.enabled + ", secondaryCam is: " + secondaryCamera.enabled);
+//		}
 	}
 }
 
