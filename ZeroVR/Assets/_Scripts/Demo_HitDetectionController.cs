f232afaeea8d25de20f6c720f9f4f98d37f8413d
Alexander Jia <jayjialex@gmail.com>
2017-04-05 21:06:32 -0400

    Fixed whatever was wrong with bullets.
    It was a stupid thing, but bleh.
    
    Also, shooting any of the obstacles in the main scene changes the color
    of said object randomly (player feedback to test collision)
    
    slightly adjusted the player's cannons to be more accurate in longer
    ranges (it's static for now, but I'm gonna make it rotate according to
    the reticle).

---
+++ ZeroVR/Assets/_Scripts/Demo_HitDetectionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo_HitDetectionController : MonoBehaviour {
+	private float timestamp; //our lovely stopwatch, keeps track of time elapsed.
+	private int lastHitDelay; //how long since an object hits said demoObject. Used for resetting demoObjects' states.
+
+	// Use this for initialization
+	void Start () {
+		lastHitDelay = 3; //after three seconds, we will reset the hit demoObject.
+	}
+	
+	/* *
+	 * Set up a timer.
+	 * When projectile hits said demoObject
+	 * change color of demoObject / send some sort of player notification of hit.
+	 * after a set amount of time, reset demoObject to initial state or whatever.
+	 * reset timer.
+	 * */
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter (Collider other) //upon collision, destroy bullet object, and change color of hit randomly.
+	{
+		if (other.gameObject.CompareTag("PlayerProjectile")) {
+			//other.gameObject.SetActive (false);
+			Destroy(other.gameObject);
+			gameObject.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value, 1.0f);
+
+		}
+	}
+}
--- ZeroVR/Assets/_Scripts/PlayerController.cs
+++ ZeroVR/Assets/_Scripts/PlayerController.cs
@@ -12,6 +12,7 @@ public class PlayerController : MonoBehaviour {
 	public GameObject leftCannon;
 	public GameObject rightCannon;
 	public GameObject bullet;
+
 //	public Camera secondaryCamera;
 	public float moveSpeed;
 	public float turnSpeed;
@@ -21,6 +22,8 @@ public class PlayerController : MonoBehaviour {
 	public int projectileSpeed;
 	private bool fireLeft;
 	private bool fireRight;
+	private float timestamp;
+	private float fireDelay;
 
 	// Use this for initialization
 	void Start () {
@@ -30,11 +33,12 @@ public class PlayerController : MonoBehaviour {
 		moveSpeed = 8;
 		turnSpeed = 6;
 		jumpPower = 16;
-		projectileSpeed = 20;
+		projectileSpeed = 500;
 		gravity = 9.8f;
 		fireLeft = false;
 		fireRight = false;
-
+		fireDelay = 0.2f;
+		debugMode = false;
 	}
 	
 	// Update is called once per frame
@@ -42,53 +46,68 @@ public class PlayerController : MonoBehaviour {
 		//Player Movement
 
 
-		if (characterController.isGrounded) {
+		if (characterController.isGrounded) { //while the player is grounded, he/she can move.
 			movementVector.y = 0; //resets the dood.
-			movementVector = (transform.forward * Input.GetAxis ("LeftJoystickY") * moveSpeed) + (transform.right * Input.GetAxis("LeftJoystickX") * moveSpeed);
-			horizontalTurn = Input.GetAxis ("RightJoystickX") * turnSpeed;
+			movementVector = (transform.forward * Input.GetAxis ("LeftJoystickY") * moveSpeed) + (transform.right * Input.GetAxis("LeftJoystickX") * moveSpeed); //controls actual movement.
 
-			transform.Rotate (new Vector3 (0, horizontalTurn, 0));
-			if (Input.GetButtonDown ("A")) {
+			if (Input.GetButtonDown ("A")) { //controls jumping.
 				movementVector.y = jumpPower;
 			}
 		}
+		horizontalTurn = Input.GetAxis ("RightJoystickX") * turnSpeed; //controls turning.
 		movementVector.y -= gravity * Time.deltaTime;
+		transform.Rotate (new Vector3 (0, horizontalTurn, 0));
 		characterController.Move(movementVector * Time.deltaTime);
 
-		if (Input.GetAxis ("LeftTrigger") > 0) {
+
+		//Other input controls.
+		if (Input.GetAxis ("LeftTrigger") > 0) {		//if left trigger is pressed, set shooting flag to true.
 			fireLeft = true;
 //			Debug.Log ("fireLeft: " + fireLeft);
 			}
-		if (Input.GetAxis ("LeftTrigger") == 0) {
+		if (Input.GetAxis ("RightTrigger") > 0) {		//if right trigger is pressed, set shooting flag to true.
+			fireRight = true;
+			//			Debug.Log ("fireRight: " + fireRight);
+		}
+		if (Input.GetAxis ("LeftTrigger") == 0) {		//if left trigger is released, set shooting flag to false.
 			fireLeft = false;
 //			Debug.Log ("fireLeft: " + fireLeft);
 		}
-		if (Input.GetAxis ("RightTrigger") > 0) {
-			fireRight = true;
-//			Debug.Log ("fireRight: " + fireRight);
-		}
-		if (Input.GetAxis ("RightTrigger") == 0) {
+		if (Input.GetAxis ("RightTrigger") == 0) {		//if right trigger is released, set shooting flag to false.
 			fireRight = false;
 //			Debug.Log ("fireRight: " + fireRight);
 		}
 
-		if(fireLeft == true)
-		{
-			fire (leftCannon);
-			//InvokeRepeating ("fire(leftCannon)", 1f, 2f);
+		if (Time.time >= timestamp) {					//Time.time controls the fire rate, so to speak. (i.e the delay between shots).
+		
+			if (fireLeft == true && fireRight == true) {		//shooting with both triggers at once.
+				fire (leftCannon);
+				fire (rightCannon);
+			}/* else if (fireRight == true && fireLeft == true) {
+				fire (leftCannon);
+				fire (rightCannon);
+			}*/ else if (fireLeft == false && fireRight == true) {		//shooting with only the right trigger.
+				fire (rightCannon);
+			} else if (fireRight == false && fireLeft == true) {		//shooting with only the left trigger.
+				fire (leftCannon);
+			}
+			timestamp = Time.time + fireDelay;					//updates the 'timestamp' variable.
 		}
-		if(fireRight == true)
-		{
-			fire (rightCannon);
-			//InvokeRepeating ("fire(rightCannon)", 1f, 2f);
-		}
+
+	//	print ("BOOL STATUS: L - " + fireLeft + ", R - " + fireRight);
 	}
 
+	/**
+	 * Actual firing/shooting function.
+	 * 
+	 **/
 	void fire(GameObject cannon)
 	{
 		GameObject projectile = Instantiate (bullet, cannon.transform.position, cannon.transform.rotation) as GameObject;
-		Rigidbody rb = bullet.GetComponent<Rigidbody>();
-		rb.AddForce (bullet.transform.forward * projectileSpeed);
+		Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+		rb.AddForce (projectile.transform.forward * projectileSpeed, ForceMode.Impulse);
+		//print ("bullet's vector is at: " + bullet.transform.forward);
 		Destroy (projectile, 10.0f);
 	}
 }
